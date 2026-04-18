4f88eb9d125a96c3a18092f72b13d1e734e144fb
SERPTO <scrow0ud@gmail.com>
2026-04-18 10:11:06 +0800

    Revert "..."
    This reverts commit 73a51c70659ef17ae04e249dd5c7b97e6120e504.

--- tempates/src/scripts/pages/Login/login-model.js
+++
@@ -1,7 +0,0 @@
-import authService from "../../services/auth-service";
-
-export default class LoginModel {
-  async login(username, password) {
-    return await authService.login(username, password);
-  }
-}
--- tempates/src/scripts/pages/register/register-model.js
+++
@@ -1,7 +0,0 @@
-import authService from "../../services/auth-service";
-
-export default class RegisterModel {
-  async register(username, phone, password) {
-    return await authService.register(username, phone, password);
-  }
-}
